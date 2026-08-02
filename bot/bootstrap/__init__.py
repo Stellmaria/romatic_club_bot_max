081c3b184534b9de1227ee254b95811da6f81314
Stellmaria <aam.cheshire@gmail.com>
2026-08-03 01:23:24 +0300

    Экспортировать route inventory API

--- bot/bootstrap/__init__.py
+++ bot/bootstrap/__init__.py
@@ -1,6 +1,11 @@
 """Application composition helpers."""
 
-from .routers import register_all_routers
+from .routers import get_router_registry, register_all_routers, route_inventory_json
 from .workers import build_background_task_specs
 
-__all__ = ["build_background_task_specs", "register_all_routers"]
+__all__ = [
+    "build_background_task_specs",
+    "get_router_registry",
+    "register_all_routers",
+    "route_inventory_json",
+]
