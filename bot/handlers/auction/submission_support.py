819186b205db8e39a7828366ec6d0a32177deaee
Stellmaria <aam.cheshire@gmail.com>
2026-08-06 09:27:58 +0300

    style: normalize submission support spacing

--- bot/handlers/auction/submission_support.py
+++ bot/handlers/auction/submission_support.py
@@ -7,7 +7,6 @@ from bot.features import auction_submission as _implementation
 auction_currency_kb = _implementation.auction_currency_kb
 compute_start_price_limits = _implementation.compute_start_price_limits
 
-
 for _name in dir(_implementation):
     if not _name.startswith("__"):
         globals()[_name] = getattr(_implementation, _name)
